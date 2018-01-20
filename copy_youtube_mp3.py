a148763ee2c675a96ee18fc03ac1adc28901d455
Kenton <kepagel@gmail.com>
2018-01-20 14:49:51 -0600

    add support for mkv files

--- copy_youtube_mp3.py
+++ copy_youtube_mp3.py
@@ -11,7 +11,7 @@ call(command.split(), shell=True)
 
 # Find name of mp4/webm file and remove spaces and unwanted characters.
 for file in os.listdir(music_downloads):
-    if file.endswith(".mp4") or file.endswith(".webm"):
+    if file.endswith(".mp4") or file.endswith(".webm") or file.endswith(".mkv"):
         first_name = file
         first_name = first_name.replace(" ", "_")
         os.renames(file, first_name)
@@ -25,7 +25,7 @@ call(command.split(), shell=True)
 
 # Delete .mp4/.webm ...it's unnessecary
 for file in os.listdir(music_downloads):
-    if file.endswith(".mp4") or file.endswith(".webm"):
+    if file.endswith(".mp4") or file.endswith(".webm") or file.endswith(".mkv"):
         os.remove(file)
 
 print ('\033[92m' + "SUCCESSFUL CONVERSION!-------------------------------------------------" + '\033[0m')
