6f977bd0020831fbe8d09b456739bcf9b7ed2c95
Pagel <pagel@Pagels-MacBook-Pro.local>
2019-12-06 19:24:58 -0600

    COmpatible with mac

--- copy_youtube_mp3.py
+++ copy_youtube_mp3.py
@@ -1,13 +1,17 @@
 import os
 from subprocess import call
-music_downloads = "C:/Users/sam/YouTube_Music/"
+#music_downloads = "C:/Users/sam/YouTube_Music/"
+music_downloads = "/Users/pagel/Desktop/Music/"
 
 os.chdir(music_downloads)
 print ('\033[92m' + "Start*******************************************************" + '\033[0m')
-youtube_url = input("Paste in youtube url: ")
+youtube_url = raw_input("Paste in youtube url: ")
+print("URL pasted was %s" % youtube_url)
 
 command = "youtube-dl --verbose " + youtube_url
-call(command.split(), shell=True)
+print("Attempting command: %s" % command)
+#call(command.split(), shell=True)
+os.system(command)
 
 print('If failing.. remember to copy the latest youtube-dl to music_downloads location')
 
@@ -23,7 +27,8 @@ print ('\033[92m' + "Attempt conversion " + first_name + '\033[0m')#print in gre
 
 # Run ffmpeg command and remove .mp4/.webm extension and convert to mp3
 command = "ffmpeg -i " + first_name + " -vn -ab 128k -ar 44100 -y " + first_name[:-5] + ".mp3"
-call(command.split(), shell=True)
+#call(command.split(), shell=True)
+os.system(command)
 
 # Delete .mp4/.webm ...it's unnessecary
 for file in os.listdir(music_downloads):
